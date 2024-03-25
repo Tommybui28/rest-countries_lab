0bb8fb809f60bf4f6e43856c4b30eb513fad3634
Tommybui28 <156359475+Tommybui28@users.noreply.github.com>
2024-03-25 20:26:49 +0000

    States intialised and JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import CountryContainer from './containers/CountryContainer';
+import VisitedCountryList from './components/VisitedCountryList';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
+  
   return (
-    
+    <div>
+      <h1>Country Bucket List</h1>
+      <VisitedCountryList visitedCountries={visitedCountries} />
+      <CountryContainer countries={countries} markAsVisited={markAsVisited} />
+    </div>
   );
 }
 
