380c45e9fb7d1e7ba411abe34aa84d38bf5df887
Tommybui28 <156359475+Tommybui28@users.noreply.github.com>
2024-03-25 21:04:38 +0000

    List sorted alphabetically

--- src/containers/CountryContainer.js
+++ src/containers/CountryContainer.js
@@ -2,6 +2,11 @@
 import Country from '../components/Country';
 
 function CountryContainer({ countries, markAsVisited, selectedCountry, handleCountrySelect }) {
+
+    // sort list alphabetically for my own santiy
+    const sortedCountries = countries.slice().sort((a, b) => {
+        return a.name.common.localeCompare(b.name.common);
+      });
     const handleSelectChange = (event) => {
       handleCountrySelect(event.target.value);
     };
@@ -11,14 +16,14 @@ function CountryContainer({ countries, markAsVisited, selectedCountry, handleCou
         <h2>All Countries</h2>
         <select value={selectedCountry} onChange={handleSelectChange}>
           <option value="">Select a country</option>
-          {countries.map(country => (
+          {sortedCountries.map(country => (
             <option key={country.cca3} value={country.cca3}>
               {country.name.common}
             </option>
           ))}
         </select>
         <ul>
-          {countries.map(country => (
+          {sortedCountries.map(country => (
             <Country key={country.cca3} country={country} markAsVisited={markAsVisited} />
           ))}
         </ul>
