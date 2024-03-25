996df2c13aad22fcb288bc97b20b225d80dbfb8c
Tommybui28 <156359475+Tommybui28@users.noreply.github.com>
2024-03-25 20:37:40 +0000

    Data fetched from API and list dropdown created

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ function App() {
   const [countries, setCountries] = useState([]);
   const [visitedCountries, setVisitedCountries] = useState([]);
   
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(response => response.json())
+      .then(data => {
+
+
+        // Set initial state with fetched country data
+        setCountries(data);
+      })
+  }, []);
+
   return (
     <div>
       <h1>Country Bucket List</h1>
--- src/containers/CountryContainer.js
+++ src/containers/CountryContainer.js
@@ -1,6 +1,32 @@
 import { useState } from 'react';
 import Country from '../components/Country';
 
+function CountryContainer({ countries, markAsVisited }) {
 
+    const [selectedCountry, setSelectedCountry] = useState('');
 
+  const handleCountrySelect = (event) => {
+    setSelectedCountry(event.target.value);
+  };
+
+  const handleVisitClick = () => {
+    if (selectedCountry) {
+      markAsVisited(selectedCountry);
+    }
+
+    return (
+      <div>
+        <h2>All Countries</h2>
+        <select>
+          <option value="">Select a country</option>
+          {countries.map(country => (
+            <option key={country.cca3} value={country.cca3}>
+              {country.name.common}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  }
+}
 export default CountryContainer;
